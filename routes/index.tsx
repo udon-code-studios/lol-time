8a99af80a631295551abcc668847bec03bebd232
Leo Battalora <leo.battalora@gmail.com>
2022-07-04 17:38:46 -0400

    change to test deno deploy

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img src="/logo.svg" height="100px" alt="the fresh logo: a sliced lemon dripping with juice" />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx file, and refresh. UPDATE!
+        Welcome to `fresh`. Try update this message in the ./routes/index.tsx file, and refresh. Deno deploy test...
       </p>
       <Counter start={3} />
     </div>
