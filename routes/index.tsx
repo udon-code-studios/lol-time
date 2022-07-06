f1c0599e2744558ee1f12a05913f140f46c35c29
Leo Battalora <leo.battalora@gmail.com>
2022-07-06 14:04:25 -0400

    homepage with search bar

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import SearchSummoner from "../components/SearchSummoner.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img src="/logo.svg" height="100px" alt="the fresh logo: a sliced lemon dripping with juice" />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx file, and refresh. Deno deploy test...
-      </p>
-      <Counter start={3} />
+    <div class={tw`p-8 mx-auto max-w-screen-md space-y-6`}>
+      <p class={tw`text-2xl font-bold`}>Get SummonerDTO by summoner name. (NA only!)</p>
+      <p>“Imagine if I had a real weapon!”</p>
+      <SearchSummoner />
     </div>
   );
 }
