ee5f5906412d3ef829509903ef6306be0d0d64e7
Leo Battalora <leo.battalora@gmail.com>
2022-07-08 00:02:34 -0400

    styling

--- components/SearchSummoner.tsx
+++ components/SearchSummoner.tsx
@@ -4,9 +4,9 @@ import { tw } from "@twind";
 
 export default function SearchSummoner() {
   return (
-    <div class={tw`flex gap-2 w-full`}>
-      <form action="/summoner">
-        <input type="text" name="name" placeholder="Summoner name..." />
+    <div class={tw`w-full`}>
+      <form action="/summoner" class={tw`flex gap-2`}>
+        <input type="text" name="name" placeholder="Summoner name..." class={tw`border-b-1`} />
         <button type="submit" class={tw`border-2 p-1`}>
           Search
         </button>
