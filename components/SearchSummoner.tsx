188fe5b0f610a28c2090a342b3343ad15bf2b525
Leo Battalora <leo.battalora@gmail.com>
2022-07-11 05:21:25 -0400

    enable region selector

--- components/SearchSummoner.tsx
+++ components/SearchSummoner.tsx
@@ -1,12 +1,28 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Platform } from "riot/api/routes.ts";
 
 export default function SearchSummoner() {
   return (
     <div class={tw`w-full`}>
       <form action="/summoner" class={tw`flex gap-2`}>
-        <input type="text" name="name" placeholder="Summoner name..." class={tw`border-b-1`} />
+        <input
+          type="text"
+          name="name"
+          placeholder="Summoner name..."
+          class={tw`border-b-1 focus:outline-none`}
+        />
+        <select name="region">
+          {Object.keys(Platform).map((key) => {
+            // default to NA1
+            return (
+              <option value={key} selected={(key === "NA1") ? true : false}>
+                {key}
+              </option>
+            );
+          })}
+        </select>
         <button type="submit" class={tw`border-2 p-1`}>
           Search
         </button>
