f4a80e484f5c087bef0878b8604f1575c5615221
Leo Battalora <leo.battalora@gmail.com>
2022-07-11 05:22:21 -0400

    custom error page routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,15 +2,19 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/summoner.tsx";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_500.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/summoner.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/joke.ts": $0,
-    "./routes/index.tsx": $1,
-    "./routes/summoner.tsx": $2,
+    "./routes/_404.tsx": $0,
+    "./routes/_500.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/summoner.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
