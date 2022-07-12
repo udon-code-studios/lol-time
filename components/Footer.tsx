9b7677129ab245c6e6df8a549cb7f369d4c90ced
leo6liu <leo.battalora@gmail.com>
2022-07-12 17:13:54 -0400

    UCS footer

---
+++ components/Footer.tsx
@@ -0,0 +1,30 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import * as icons from "./icons.tsx";
+
+export default function Footer() {
+  return (
+    <div class={tw`w-full flex flex-col gap-2`}>
+      {/* <div class={tw`w-full border-1 border-current`} /> */}
+      <div class={tw`w-full flex justify-between items-end gap-8`}>
+        <a href="https://udon.studio" target="_blank">
+          <div>
+            <icons.UdonLogoBgBlack class={tw`w-24 dark:w-0`} />
+            <icons.UdonLogoBgWhite class={tw`w-0 dark:w-24`} />
+          </div>
+        </a>
+
+        <div class={tw`flex gap-4 pr-4`}>
+          <a href="https://github.com/udon-code-studios/lol-time" class={tw`hover:underline`}>GitHub</a>
+          <a href="https://github.com/udon-code-studios/lol-time" class={tw`hover:underline`}>Email</a>
+          <a href="https://github.com/udon-code-studios/lol-time" class={tw`hover:underline`}>Twitter</a>
+        </div>
+      </div>
+      <div class={tw`w-full border-1 border-current`} />
+      <p class={tw`text-xs`}>
+        Copyright © 2022 Udon Code Studios. All rights reserved.
+      </p>
+    </div>
+  );
+}
