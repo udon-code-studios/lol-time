3361ab67138fef63602941be649ea753a9ec427d
leo6liu <leo.battalora@gmail.com>
2022-07-12 17:14:25 -0400

    fixed colors

--- components/SearchSummoner.tsx
+++ components/SearchSummoner.tsx
@@ -2,8 +2,9 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import { Platform } from "riot/api/routes.ts";
+import * as icons from "./icons.tsx";
 
-export default function SearchSummoner() {
+export default function SearchSummoner(props: { hideSearch?: boolean }) {
   return (
     <div class={tw`w-full`}>
       <form action="/summoner" class={tw`flex gap-2`}>
@@ -11,9 +12,9 @@ export default function SearchSummoner() {
           type="text"
           name="name"
           placeholder="Summoner name..."
-          class={tw`border-b-1 focus:outline-none`}
+          class={tw`border-b-1 focus:outline-none dark:bg-black`}
         />
-        <select name="region">
+        <select name="region" class={tw`h-8 dark:text-black`}>
           {Object.keys(Platform).map((key) => {
             // default to NA1
             return (
@@ -23,8 +24,8 @@ export default function SearchSummoner() {
             );
           })}
         </select>
-        <button type="submit" class={tw`border-2 p-1`}>
-          Search
+        <button type="submit" hidden={props.hideSearch} class={tw`p-1`}>
+          <icons.PhMagnifyingGlassBold class={tw`w-6 text-gray-300 dark:text-current`} />
         </button>
       </form>
     </div>
