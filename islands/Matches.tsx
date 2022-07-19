5691de9ce2909732d6a96b4f79dd1e9d9c8ee3a1
leo6liu <leo.battalora@gmail.com>
2022-07-18 21:55:06 -0400

    loading state

--- islands/Matches.tsx
+++ islands/Matches.tsx
@@ -7,14 +7,30 @@ import Match from "../islands/Match.tsx";
 
 export default function Matches(props: { puuid: string; region: keyof typeof riot.routes.Platform }) {
   const [matchIds, setMatchIds] = useState<{ status: string; matches?: string[] }>();
+  const [loadingState, setLoadingState] = useState<"loading" | "complete" | "failed">("loading");
 
   useEffect(() => {
+    setLoadingState("loading");
     fetch(`/api/matches?puuid=${props.puuid}&region=${props.region}`).then((res) => {
       res.json().then((data) => {
         setMatchIds(data);
+        setLoadingState("complete");
+      }).catch(() => {
+        console.log("[ ERROR ] Failed to load matches.");
+        setLoadingState("failed");
       });
     });
   }, [props]);
+
+  if (loadingState === "loading") {
+    return <div class={tw`w-full flex flex-col justify-center gap-4 text-center`}>loading matches...</div>;
+  }
+
+  if (loadingState === "failed") {
+    return (
+      <div class={tw`w-full flex flex-col justify-center gap-4 text-center`}>[ ERROR ] Failed to load matches.</div>
+    );
+  }
 
   return (
     <div class={tw`w-full flex flex-col justify-center gap-4`}>
