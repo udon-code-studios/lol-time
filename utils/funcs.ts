cbf93acd610d216bd4bdd68454a844e557b176cd
Leo <leo.battalora@gmail.com>
2022-10-08 23:33:24 -0400

    format seconds to minutes and seconds

--- utils/funcs.ts
+++ utils/funcs.ts
@@ -2,3 +2,15 @@
 export function capitalize(str: string): string {
   return str.toLowerCase().replace(new RegExp("\\b\\w", "g"), (c) => c.toUpperCase());
 }
+
+/** Converts number of seconds to 'MMm SSs' format */
+export function formatDuration(duration: number | undefined): string {
+  if (duration === undefined) {
+    return `0m 0s`;
+  }
+
+  const minutes = Math.floor(duration / 60);
+  const seconds = duration - minutes * 60;
+
+  return `${minutes}m ${seconds}s`;
+}
